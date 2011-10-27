f70ce7feab938a4407c5c297757c89f9192bc118
Firstname Lastname <gjero@krsteski.de>
2011-10-27 16:48:31 +0200

    write documentation

--- src/framework/IdentityMap.php
+++ src/framework/IdentityMap.php
@@ -1,4 +1,5 @@
 <?php
+use Doctrine\DBAL\Types\BooleanType;
 class IdentityMap
 {
     protected $idToObject = array();
@@ -9,17 +10,21 @@ class IdentityMap
         $this->objectToId = new SplObjectStorage();
     }
 
+    /**
+     * @param integer $id
+     * @param object $object
+     */
     public function set($id, $object)
     {
         $this->idToObject[$id]     = $object;
         $this->objectToId[$object] = $id;
     }
 
-    public function hasObject($object)
-    {
-        return isset($this->objectToId[$object]);
-    }
-
+    /**
+     * @param object $object
+     * @throws OutOfBoundsException
+     * @return integer
+     */
     public function getId($object)
     {
         if (false === $this->hasObject($object))
@@ -30,11 +35,29 @@ class IdentityMap
         return $this->objectToId[$object];
     }
 
+    /**
+     * @param integer $id
+     * @return booelan
+     */
     public function hasId($id)
     {
         return isset($this->idToObject[$id]);
     }
 
+    /**
+     * @param object $object
+     * @return boolean
+     */
+    public function hasObject($object)
+    {
+        return isset($this->objectToId[$object]);
+    }
+
+    /**
+     * @param integer $id
+     * @throws OutOfBoundsException
+     * @return object
+     */
     public function getObject($id)
     {
         if (false === $this->hasId($id))
